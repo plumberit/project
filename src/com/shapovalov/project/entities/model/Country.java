469c684d4c90cdbc0704097c2fd5651217809dc9
plumberit <dshapovalov2001@gmail.com>
2022-12-17 22:18:28 +0200

    Add getters and setters to Country

--- src/com/shapovalov/project/entities/model/Country.java
+++ src/com/shapovalov/project/entities/model/Country.java
@@ -7,7 +7,39 @@ public class Country {
     private int square;
     private long UDP;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public int getSquare() {
+        return square;
+    }
+
+    public long getUDP() {
+        return UDP;
+    }
+
     public void setId(int id) {
         this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+    public void setSquare(int square) {
+        this.square = square;
+    }
+    public void setUDP(long UDP) {
+        this.UDP = UDP;
     }
 }
