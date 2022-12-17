9952dee87029a35cc16a15280b1745ee5b1cb292
plumberit <dshapovalov2001@gmail.com>
2022-12-17 17:18:21 +0200

    Create Model Union

---
+++ src/com/shapovalov/project/model/Union.java
@@ -0,0 +1,9 @@
+package com.shapovalov.project.model;
+
+import java.util.ArrayList;
+
+public class Union {
+    private int id;
+    private String name;
+    private ArrayList <Country> union = new ArrayList<>();
+}
