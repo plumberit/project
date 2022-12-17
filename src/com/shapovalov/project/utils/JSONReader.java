869889145faf9100d86f5b35d2024e8e80c84b4f
plumberit <dshapovalov2001@gmail.com>
2022-12-17 18:06:30 +0200

    create method fillCountries() in JSONReader

--- src/com/shapovalov/project/utils/JSONReader.java
+++ src/com/shapovalov/project/utils/JSONReader.java
@@ -1,18 +1,36 @@
 package com.shapovalov.project.utils;
 
 import com.shapovalov.project.entities.model.Country;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class JSONReader {
 
     //временная копия
     ArrayList<Country> countries = new ArrayList<>();
+
+    public void init() {
+        fillCountries(JSONCreator(databaseReader()));
+    }
+
+    void fillCountries(JSONObject obj) {
+        JSONArray arr = (JSONArray) obj.get("countries 2020");
+        Iterator iterator = arr.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            countries.add(new Country());
+            JSONObject object = (JSONObject) iterator.next();
+            countries.get(i).setId((int) object.get("id"));
+            i++;
+        }
+    }
 
     private static String databaseReader() {
         StringBuilder str = new StringBuilder();
