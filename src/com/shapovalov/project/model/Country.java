8c852370e9a388496b3c5bd2e06b08f50d4d48d2
plumberit <dshapovalov2001@gmail.com>
2022-12-17 17:15:59 +0200

    Create Model Country

---
+++ src/com/shapovalov/project/model/Country.java
@@ -0,0 +1,9 @@
+package com.shapovalov.project.model;
+
+public class Country {
+    private int id;
+    private String name;
+    private long population;
+    private int square;
+    private long UDP;
+}
