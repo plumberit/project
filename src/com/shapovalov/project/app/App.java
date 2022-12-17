3d281037b16d550c788d58399d9aa91e32588c6b
plumberit <dshapovalov2001@gmail.com>
2022-12-17 17:04:55 +0200

    Create data.json

--- src/com/shapovalov/project/app/App.java
+++ src/com/shapovalov/project/app/App.java
@@ -6,7 +6,6 @@ import com.shapovalov.project.app.layers.ProcessLayer;
 public class App {
 
     public void run() {
-
         InitializeLayer initializeLayer = new InitializeLayer();
         initializeLayer.run();
         ProcessLayer processLayer = new ProcessLayer();
--- src/com/shapovalov/project/app/layers/InitializeLayer.java
+++ src/com/shapovalov/project/app/layers/InitializeLayer.java
@@ -4,5 +4,6 @@ public class InitializeLayer {
 
     public void run() {
 
+
     }
 }
