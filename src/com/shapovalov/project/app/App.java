ff2972b4cc5512e982a8c7b6ffeba4133e87b003
plumberit <dshapovalov2001@gmail.com>
2022-12-17 13:40:18 +0200

    Modify AppLayer

--- src/com/shapovalov/project/app/App.java
+++ src/com/shapovalov/project/app/App.java
@@ -2,12 +2,18 @@ package com.shapovalov.project.app;
 
 public class App {
 
+    public void run() {
+        appLayerSwitcher();
+    }
+
     enum AppLayer {
         START_LAYER,
         PROCESS_LAYER;
     }
 
-    public void run() {
+    AppLayer appLayer = AppLayer.START_LAYER;
+
+    AppLayer appLayerSwitcher() {
 
     }
 }
