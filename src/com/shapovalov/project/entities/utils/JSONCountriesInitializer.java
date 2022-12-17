5e6f3f8978b94774bb1958521dc82560609e78b5
plumberit <dshapovalov2001@gmail.com>
2022-12-17 18:19:47 +0200

    Create JSONCountriesInitializer

---
+++ src/com/shapovalov/project/entities/utils/JSONCountriesInitializer.java
@@ -0,0 +1,26 @@
+package com.shapovalov.project.entities.utils;
+
+import com.shapovalov.project.entities.model.Country;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class JSONCountriesInitializer {
+    //временная копия
+    ArrayList<Country> countries = new ArrayList<>();
+    initializeList(JSONObject);
+
+    void initializeList(JSONObject obj) {
+        JSONArray arr = (JSONArray) obj.get("countries 2020");
+        Iterator iterator = arr.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            countries.add(new Country());
+            JSONObject object = (JSONObject) iterator.next();
+            countries.get(i).setId((int) object.get("id"));
+            i++;
+        }
+    }
+}
--- src/com/shapovalov/project/utils/JSONReader.java
+++ src/com/shapovalov/project/utils/JSONReader.java
@@ -1,24 +1,17 @@
 package com.shapovalov.project.utils;
 
-import com.shapovalov.project.entities.model.Country;
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class JSONReader {
 
-    //временная копия
-    ArrayList<Country> countries = new ArrayList<>();
-
     public void init() {
         String JSONFileText = JSONFileReader();
         JSONObject JSONObject = JSONObjectCreator(JSONFileText);
-        initializeList(JSONObject);
     }
 
     private static String JSONFileReader() {
@@ -45,17 +38,5 @@ public class JSONReader {
             e.printStackTrace();
         }
         return obj;
-    }
-
-    void initializeList(JSONObject obj) {
-        JSONArray arr = (JSONArray) obj.get("countries 2020");
-        Iterator iterator = arr.iterator();
-        int i = 0;
-        while (iterator.hasNext()) {
-            countries.add(new Country());
-            JSONObject object = (JSONObject) iterator.next();
-            countries.get(i).setId((int) object.get("id"));
-            i++;
-        }
     }
 }
