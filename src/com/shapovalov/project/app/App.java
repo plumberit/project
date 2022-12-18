5b2891d99b9068fa80a095866b4d375ef44eff57
plumberit <dshapovalov2001@gmail.com>
2022-12-18 14:31:04 +0200

    Create StartLayer

--- src/com/shapovalov/project/app/App.java
+++ src/com/shapovalov/project/app/App.java
@@ -2,12 +2,15 @@ package com.shapovalov.project.app;
 
 import com.shapovalov.project.app.layers.InitializeLayer;
 import com.shapovalov.project.app.layers.ProcessLayer;
+import com.shapovalov.project.app.layers.StartLayer;
 
 public class App {
 
     public void run() {
         InitializeLayer initializeLayer = new InitializeLayer();
         initializeLayer.run();
+        StartLayer startLayer = new StartLayer();
+        startLayer.run();
         ProcessLayer processLayer = new ProcessLayer();
         processLayer.run();
         }
---
+++ src/com/shapovalov/project/app/layers/StartLayer.java
@@ -0,0 +1,7 @@
+package com.shapovalov.project.app.layers;
+
+public class StartLayer {
+    public void run() {
+        
+    }
+}
