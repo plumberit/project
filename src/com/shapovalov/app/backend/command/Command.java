95ae72547cb8ddf85dda90141a6402761f56750b
plumberit <dshapovalov2001@gmail.com>
2022-12-19 14:58:43 +0200

    Create Command Model

---
+++ src/com/shapovalov/app/backend/command/Command.java
@@ -0,0 +1,22 @@
+package com.shapovalov.app.backend.command;
+
+public class Command {
+    private String command;
+    private String parameter;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+}
--- src/com/shapovalov/app/backend/command/CommandFormatter.java
+++ src/com/shapovalov/app/backend/command/CommandFormatter.java
@@ -2,6 +2,6 @@ package com.shapovalov.app.backend.command;
 
 public class CommandFormatter {
     void format(String command) {
-
+        
     }
 }
