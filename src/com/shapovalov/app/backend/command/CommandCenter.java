01de1bc7d0f26b3231efc1ad86299687a0fedac3
plumberit <dshapovalov2001@gmail.com>
2022-12-19 14:48:09 +0200

    Create Command Center

---
+++ src/com/shapovalov/app/backend/command/CommandCenter.java
@@ -0,0 +1,8 @@
+package com.shapovalov.app.backend.command;
+
+public class CommandCenter {
+
+    public void handle(String command) {
+
+    }
+}
--- src/com/shapovalov/app/base/layers/logic/AppCycleProcess.java
+++ src/com/shapovalov/app/base/layers/logic/AppCycleProcess.java
@@ -1,11 +1,13 @@
 package com.shapovalov.app.base.layers.logic;
 
+import com.shapovalov.app.backend.command.CommandCenter;
 import com.shapovalov.app.view.View;
 
 public class AppCycleProcess {
     public void run() {
         View view = new View();
         String input = view.getInput();
-
+        CommandCenter commandCenter = new CommandCenter();
+        commandCenter.handle(input);
     }
 }
