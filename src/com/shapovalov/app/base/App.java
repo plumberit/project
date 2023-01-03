b5e5c4bb3b48b9b9eec69b6c2d5114a3df5aee3c
plumberit <dshapovalov2001@gmail.com>
2023-01-03 15:50:40 +0200

    Optimize memory use in App sequence

--- src/com/shapovalov/app/base/App.java
+++ src/com/shapovalov/app/base/App.java
@@ -8,18 +8,23 @@ import com.shapovalov.app.base.components.StartComponent;
 public class App {
 
     public void run() {
+        runInitializeComponent();
+        runStartComponent();
+        runProcessComponent();
+    }
 
-        //объявление компонентов программы
-        AppComponent initializeComponent, startComponent, processComponent;
-
-        //инициализация компонентов
-        initializeComponent = new InitializeComponent();
-        startComponent = new StartComponent();
-        processComponent = new ProcessComponent();
-
-        //последовательное исполнение компонентов
+    private void runInitializeComponent() {
+        AppComponent initializeComponent = new InitializeComponent();
         initializeComponent.run();
+    }
+
+    private void runStartComponent() {
+        AppComponent startComponent = new StartComponent();
         startComponent.run();
+    }
+
+    private void runProcessComponent() {
+        AppComponent processComponent = new ProcessComponent();
         processComponent.run();
     }
 }
