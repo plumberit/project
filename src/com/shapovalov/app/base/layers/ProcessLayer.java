2685329a023534822af7e11ec4bbeb8e98294274
plumberit <dshapovalov2001@gmail.com>
2023-01-03 15:18:58 +0200

    Simplify ProcessSubLayer to ProccessLayer content

--- src/com/shapovalov/app/base/layers/ProcessLayer.java
+++ src/com/shapovalov/app/base/layers/ProcessLayer.java
@@ -1,13 +1,28 @@
 package com.shapovalov.app.base.layers;
 
-import com.shapovalov.app.base.layers.sublayers.ProcessSubLayer;
+import com.shapovalov.app.backend.function.FunctionCenter;
+import com.shapovalov.app.model.Result;
+import com.shapovalov.app.view.Display;
+import com.shapovalov.app.view.View;
 
 public class ProcessLayer implements AppComponent {
 
     public void run() {
         while (true) {
-            AppComponent appProcess = new ProcessSubLayer();
-            appProcess.run();
+            Display display = new Display();
+            String input = display.input();
+
+            FunctionCenter functionCenter = new FunctionCenter();
+            Result result = functionCenter.handle(input);
+
+            View view = new View();
+            if(result.isError()) {
+                view.setError(result.getStrokes());
+            } else {
+                view.setResult(result.getStrokes());
+            }
+
+            display.output(view.getStrokes());
         }
     }
 }
