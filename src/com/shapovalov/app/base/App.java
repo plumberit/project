de4fbba9170b7d7f166aea52816d7180afb3456b
plumberit <dshapovalov2001@gmail.com>
2023-01-03 15:54:26 +0200

    Simplify App sequence

--- src/com/shapovalov/app/base/App.java
+++ src/com/shapovalov/app/base/App.java
@@ -8,23 +8,12 @@ import com.shapovalov.app.base.components.StartComponent;
 public class App {
 
     public void run() {
-        runInitializeComponent();
-        runStartComponent();
-        runProcessComponent();
+        runComponent(new InitializeComponent());
+        runComponent(new StartComponent());
+        runComponent(new ProcessComponent());
     }
 
-    private void runInitializeComponent() {
-        AppComponent initializeComponent = new InitializeComponent();
-        initializeComponent.run();
-    }
-
-    private void runStartComponent() {
-        AppComponent startComponent = new StartComponent();
-        startComponent.run();
-    }
-
-    private void runProcessComponent() {
-        AppComponent processComponent = new ProcessComponent();
-        processComponent.run();
+    private void runComponent(AppComponent component) {
+        component.run();
     }
 }
